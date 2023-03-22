638713e25a4b6a22429da7baca6055e9e428cf68
dennohkim <kimathidennis38@gmail.com>
2023-03-22 17:15:31 +0300

    create websocket connectionand conet with the api

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
-import './App.css'
+import { useEffect, useState } from 'react';
+import { WsProvider, ApiPromise } from '@polkadot/api';
 
 function App() {
-  
+  const [api, setApi] = useState<ApiPromise | null>(null)
+  const setup = async () => {
+    const wsprovider = new WsProvider('wss://rpc.polkadot.io')
+    const api = await ApiPromise.create({ provider: wsprovider  })
+    setApi(api)
+  }
+
+  useEffect(() => {
+    setup()
+  }, [])
+
+  useEffect(() => {
+    if(!api) return
+
+    (async() => {
+      const time = await api.query.timestamp.now()
+      console.log(time.toPrimitive())
+    })()
+  }, [api])
 
   return (
     <div className="App">
