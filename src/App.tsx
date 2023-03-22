30d6370728fdd992baba271013ce808ab249d87a
dennohkim <kimathidennis38@gmail.com>
2023-03-22 19:05:04 +0300

    wallet connection

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,51 @@
 import { useEffect, useState } from 'react';
 import { WsProvider, ApiPromise } from '@polkadot/api';
+import { web3Enable } from '@polkadot/extension-dapp';
+import { web3Accounts } from '@polkadot/extension-dapp/bundle';
+import { InjectedAccountWithMeta } from '@polkadot/extension-inject/types';
 
+
+const NAME = 'tenderspace';
 function App() {
-  const [api, setApi] = useState<ApiPromise | null>(null)
+  const [api, setApi] = useState<ApiPromise>()
+  const [accounts, setAccounts] = useState<InjectedAccountWithMeta[]>([])
+  const [selectedAccount, setSelectedAccount] = useState<InjectedAccountWithMeta>();
+
+
+
   const setup = async () => {
     const wsprovider = new WsProvider('wss://rpc.polkadot.io')
     const api = await ApiPromise.create({ provider: wsprovider  })
     setApi(api)
+  }
+
+  const handleConnection = async() => {
+    //Check if user has connection
+    const extensions = await web3Enable(NAME)
+
+    if (!extensions) {
+        throw Error("NO_EXTENSION_FOUND")
+    }
+
+    const allAccounts = await web3Accounts();
+    setAccounts(allAccounts)
+
+    if (allAccounts.length === 1) {
+        setSelectedAccount(allAccounts[0])
+    }
+  }
+
+  const handleAccountSelection = async(e: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedAddress = e.target.value
+
+    const account = accounts.find(account => account.address === selectedAddress)
+
+    if(!account) {
+      throw Error("ACCOUNT_NOT_FOUND")
+    }
+
+    setSelectedAccount(account)
+
   }
 
   useEffect(() => {
@@ -23,10 +62,25 @@ function App() {
   }, [api])
 
   return (
-    <div className="App">
-      
+    <div>
+      {accounts.length === 0 ? (
+        <button onClick={handleConnection}>CONNECT</button>
+      ) : (
+        <button onClick={handleConnection}>CONNECTED</button>
+      )}
+      {accounts.length > 0 && !selectedAccount ? (
+        <>
+          <select onChange={handleAccountSelection}>
+            <option disabled selected hidden>Choose your account</option>
+            {accounts.map((account) => (
+              <option value={account.address}>{account.address}</option>
+            ))}
+          </select>
+        </>
+      ) : null}
+      {selectedAccount ? selectedAccount.address : null}
     </div>
-  )
+  );
 }
 
 export default App
